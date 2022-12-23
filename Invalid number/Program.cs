4a18978fd2e93bc4065efd1d8608ebe3256b4877
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-12-23 11:25:51 +0200

    Add files via upload

---
+++ Invalid number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Invalid_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                if (number < 100 || number > 200) Console.WriteLine("invalid");
+            }
+        }
+    }
+}
