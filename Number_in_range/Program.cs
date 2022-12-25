094c6e655685f50799a732bb048c5b63d96c242d
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-12-25 11:41:07 +0200

    Add files via upload

---
+++ Number_in_range/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Number_in_range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                if (number >= -100 && number <= 100) Console.WriteLine("Yes");
+                else Console.WriteLine("No");
+            }
+            else Console.WriteLine("No");
+        }
+    }
+}
