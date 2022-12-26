ad6864f51408d6360c70f89f18b8415f7f7609a7
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-12-26 08:22:02 +0200

    Add files via upload

---
+++ Working_hours/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Working_hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
