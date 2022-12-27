aaae757ae27e7ae745f4fc6a52f6b25449705afb
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-12-27 09:29:47 +0200

    Add files via upload

---
+++ Working_time/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Working_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (day != "Sunday")
+            {
+                if (hour >= 10 && hour <= 18) Console.WriteLine("open");
+                else Console.WriteLine("closed");
+            }
+            else Console.WriteLine("closed");
+        }
+    }
+}
