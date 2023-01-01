b4110eb5236b78c05724b75fcded540d916d13ef
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-01-01 17:48:43 +0200

    Add files via upload

---
+++ Cinema/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projection = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+
+            switch (projection)
+            {
+                case "Premiere":
+                    Console.WriteLine($"{(12*rows*columns):f2} leva");
+                    break;
+                case "Normal":
+                    Console.WriteLine($"{(7.5 * rows * columns):f2} leva");
+                    break;
+                case "Discount":
+                    Console.WriteLine($"{(5 * rows * columns):f2} leva");
+                    break;
+                default: Console.WriteLine("error");
+                    break;
+            }
+        }
+    }
+}
