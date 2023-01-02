cd82fdfbb16233c3fa903104af0b1adfac9815f6
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-01-02 09:35:54 +0200

    Add files via upload

---
+++ Lunch Break/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lunch_Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double episode = double.Parse(Console.ReadLine());
+            double break_time = double.Parse(Console.ReadLine());
+
+            double time_lunch = break_time / 8;
+            double time_rest = break_time / 4;
+            double break_total = break_time - (time_rest + time_lunch);
+
+
+            if (break_total >= episode) Console.WriteLine($"You have enough time to watch {name} and left with {Math.Ceiling(break_total-episode)} minutes free time.");
+            else Console.WriteLine($"You don't have enough time to watch {name}, you need {Math.Ceiling(episode-break_total)} more minutes.");
+        }
+    }
+}
