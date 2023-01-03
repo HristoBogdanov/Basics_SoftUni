6a0a71270e6799c51164b02b3fcc2dc17bcd3600
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-01-03 09:13:12 +0200

    Add files via upload

---
+++ Swimming Record/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Swimming_Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double time = double.Parse(Console.ReadLine());
+
+            double total_time = distance * time;
+            double time_delay = Math.Floor(distance / 15) * 12.5;
+            double result_time = total_time + time_delay;
+
+            if (result_time < record) Console.WriteLine($" Yes, he succeeded! The new world record is {result_time:f2} seconds.");
+            else Console.WriteLine($"No, he failed! He was {(result_time-record):f2} seconds slower.");
+        }
+    }
+}
