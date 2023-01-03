38c20d89ed61d748ddea1667b1c4134cb957ce92
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-01-03 09:11:56 +0200

    Add files via upload

---
+++ Running Times/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Running_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firsttime = int.Parse(Console.ReadLine());
+            int secondtime = int.Parse(Console.ReadLine());
+            int thirdtime = int.Parse(Console.ReadLine());
+
+            int total = firsttime + secondtime + thirdtime;
+
+            int minutes = total / 60;
+            int seconds = total % 60;
+
+            if (seconds >= 10) Console.WriteLine($"{minutes}:{seconds}");
+            else Console.WriteLine($"{minutes}:0{seconds}");
+        }
+    }
+}
