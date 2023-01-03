cfa4672102e41136898975361bbe6543e29481a4
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-01-03 09:12:37 +0200

    Add files via upload

---
+++ Shopping/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int videocards = int.Parse(Console.ReadLine());
+            int procesors= int.Parse(Console.ReadLine());
+            int ram = int.Parse(Console.ReadLine());
+
+            double total_videocards = videocards * 250;
+            double total_procesors = (total_videocards * 0.35) * procesors;
+            double total_ram = (total_videocards * 0.1) * ram;
+
+            double total = total_procesors + total_ram + total_videocards;
+
+            if (videocards > procesors) total = total - (total * 0.15);
+
+            if (total <= budget) Console.WriteLine($"You have {budget-total:f2} leva left!");
+            else Console.WriteLine($"Not enough money! You need {total-budget:f2} leva more!");
+        }
+    }
+}
