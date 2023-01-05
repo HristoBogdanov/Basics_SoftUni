4403452c82f5473a9c64d0d6fe09f651789205ef
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-01-05 15:02:39 +0200

    Add files via upload

---
+++ Between100and200/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Between100and200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a >= 100 && a <= 200) Console.WriteLine("Between 100 and 200");
+            else if (a < 100) Console.WriteLine("Less than 100");
+            else Console.WriteLine("Greater than 200");
+        }
+    }
+}
