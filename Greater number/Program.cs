fb8c464dd5910e0968200c635d99ea742b65595c
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-01-07 11:54:09 +0200

    Add files via upload

---
+++ Greater number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Greater_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a >= b) Console.WriteLine(a);
+            else Console.WriteLine(b);
+        }
+    }
+}
