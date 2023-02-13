4492d1c4e277395184cb7c5fe3c7e5e35fa266f0
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-02-13 08:56:39 +0200

    Add files via upload

---
+++ Concat Data/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Concat_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = Console.ReadLine();
+            string secondname= Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+
+            Console.WriteLine("You are " + firstname + " " +secondname + ", a " + age + "-years old person from " + town +".");
+        }
+    }
+}
