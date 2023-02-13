77a682cdf87c382e95a789a16b12daf8c627a7ea
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-02-13 08:57:05 +0200

    Add files via upload

---
+++ Creating Projects/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Creating_Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int number_projects = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The architect {name} will need {number_projects*3} hours to complete {number_projects} project/s.");
+        }
+    }
+}
