f22c468164e12ce910e50ccefc85029d0789c233
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-02-15 08:41:27 +0200

    Add files via upload

---
+++ Pet Shop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pet_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers_dogs = int.Parse(Console.ReadLine());
+            int numbers_cats = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{(numbers_cats*4) + (numbers_dogs*2.5)} lv.");
+        }
+    }
+}
