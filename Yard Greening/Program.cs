fad5d1717d7642c15b8faa19339dd32a823317f2
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-02-15 08:42:16 +0200

    Add files via upload

---
+++ Yard Greening/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Yard_Greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double yards = double.Parse(Console.ReadLine());
+
+            double discount = 0.18 * (yards * 7.61);
+            double result = (yards * 7.61) - discount;
+
+            Console.WriteLine($"The final price is: {result} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
