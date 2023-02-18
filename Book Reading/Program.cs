a6b05fc8992c78f9efd52f3d58e374b707c7c8df
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-02-18 07:43:39 +0200

    Add files via upload

---
+++ Book Reading/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Book_Reading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count_pages = int.Parse(Console.ReadLine());
+            int pages_per_day = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            int time = count_pages / pages_per_day;
+            int result = time / days;
+            Console.WriteLine(result);
+        }
+    }
+}
