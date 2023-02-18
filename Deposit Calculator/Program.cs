d1ccad0ed4cb28fba133978246f2a45d9033e5b4
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-02-18 07:44:03 +0200

    Add files via upload

---
+++ Deposit Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = double.Parse(Console.ReadLine());
+            int srok = int.Parse(Console.ReadLine());
+            double interest = double.Parse(Console.ReadLine());
+
+            double result_interest = sum * (interest / 100);
+            double interest_month = result_interest / 12;
+            double result = sum + (srok * interest_month);
+
+            Console.WriteLine(result);
+        }
+    }
+}
