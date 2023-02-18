cedbc00be8504c4467e32c814bfdccee8586c910
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-02-18 07:43:09 +0200

    Add files via upload

---
+++ Basketball equpment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Basketball_equpment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tax = int.Parse(Console.ReadLine());
+
+            double shoes = tax - tax * 0.4;
+            double shirt = shoes - shoes * 0.2;
+            double ball = shirt * 0.25;
+            double accessories = ball * 0.2;
+
+            double result = tax + shoes + shirt + ball + accessories;
+            Console.WriteLine(result);
+        }
+    }
+}
