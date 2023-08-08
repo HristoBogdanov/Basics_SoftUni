c06562a4538006edf0001ea1193672db511c35fd
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-08-08 14:23:00 +0300

    Add files via upload

---
+++ KSI_2016/KSI_2016/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KSI_2016
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student student = new Student(textBox1.Text, textBox2.Text, double.Parse(textBox3.Text), textBox4.Text);
+            students.Add(student);
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sorted_students = students.OrderBy(x => x.Speciality).ThenByDescending(x => x.CalculateBal());
+            foreach (Student x in sorted_students) listBox1.Items.Add(x.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<Student> students_speciality = new List<Student>();
+
+            foreach (Student x in students) if (x.Speciality == textBox5.Text && x.Form == textBox6.Text) students_speciality.Add(x);
+
+            var sorted_students_2 = students_speciality.OrderBy(x => x.Name_student);
+
+            foreach (Student x in sorted_students_2) listBox1.Items.Add(x.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            List<Student> students_speciality = new List<Student>();
+
+            foreach (Student x in students) if (x.Speciality == textBox5.Text && x.Form == textBox6.Text) students_speciality.Add(x);
+
+            double sum = 0;
+            foreach (Student x in students_speciality) sum += x.CalculateBal();
+            listBox1.Items.Add($"{(sum / students_speciality.Count):f2}");
+        }
+    }
+}
---
+++ KSI_2016/KSI_2016/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSI_2016
+{
+    public class Student
+    {
+        private string name_student;
+        private string speciality;
+        private double grade;
+        private string form;
+
+        public string Name_student
+        {
+            get { return this.name_student; }
+            set
+            {
+                if (value.Length > 30) throw new ArgumentException("Invalid data");
+                else this.name_student = value;
+            }
+        }
+
+        public string Speciality
+        {
+            get { return this.speciality; }
+            set 
+            {
+                if (value.Length > 20) throw new ArgumentException("Invalid data");
+                else this.speciality = value;
+            }
+        }
+
+        public double Grade
+        {
+            get { return this.grade; }
+            set {
+                if (value < 2.0 || value > 6.0) throw new ArgumentException("Invalid data");
+                else this.grade = value;
+            }
+        }
+
+        public string Form
+        {
+            get { return this.form; }
+            set
+            {
+                if (value.Length > 15) throw new ArgumentException("Invalid data");
+                else this.form = value;
+            }
+        }
+
+        public Student(string name, string speciality,double grade,string form)
+        {
+            this.Name_student = name;
+            this.Speciality = speciality;
+            this.Grade = grade;
+            this.Form = form;
+        }
+
+        public double CalculateBal()
+        {
+            return this.Grade * 6;
+        }
+        public override string ToString()
+        {
+            return $"{this.Name_student}; {this.Speciality}; {this.Form}; {this.CalculateBal()}";
+        }
+    }
+}
