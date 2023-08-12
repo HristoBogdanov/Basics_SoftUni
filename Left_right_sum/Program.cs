ed994b551c1195daf3f52bfd6a281c4855430156
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-08-12 14:52:22 +0300

    Add files via upload

---
+++ Left_right_sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Left_right_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <count*2; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                numbers.Add(a);
+            }
+            int sum_left = 0;
+            int sum_right = 0;
+
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                sum_left += numbers[i];
+            }
+            for(int i=numbers.Count-1;i>=numbers.Count/2;i--)
+            {
+                sum_right += numbers[i];
+            }
+
+            if (sum_right == sum_left) Console.WriteLine($"Yes, sum = {sum_left}");
+            else if (sum_left > sum_right) Console.WriteLine($"No, diff = {sum_left-sum_right}");
+            else Console.WriteLine($"No, diff = {sum_right - sum_left}");
+        }
+    }
+}
---
+++ odd_even/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace odd_even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int>arr = new List<int>();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                arr.Add(a);
+            }
+            int sum_odds = 0;
+            int sum_evens = 0;
+            for(int i=0;i<arr.Count;i=i+2)
+            {
+                sum_evens += arr[i];
+            }
+
+            for (int i = 1; i < arr.Count; i = i + 2)
+            {
+                sum_odds += arr[i];
+            }
+            if (sum_evens == sum_odds)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sum_odds}");
+            }
+            else if (sum_evens > sum_odds)
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {sum_evens - sum_odds}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {sum_odds - sum_evens}");
+            }
+        }
+    }
+}
