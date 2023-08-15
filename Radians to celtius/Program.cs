7da1076cb4d82e64bf5813c96e55a60c070de045
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-08-15 16:35:16 +0300

    Add files via upload

---
+++ Radians to celtius/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Radians_to_celtius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radian = double.Parse(Console.ReadLine());
+
+            double celtius = radian * 180 / Math.PI;
+
+            Console.WriteLine(celtius);
+        }
+    }
+}
---
+++ Repainting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nailon = int.Parse(Console.ReadLine());
+            int boq = int.Parse(Console.ReadLine());
+            int razreditel = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+
+            double sum_nailon = (nailon + 2) * 1.50;
+            double sum_boq = (boq + boq * 0.1) * 14.5;
+            double sum_razreditel = razreditel * 5;
+
+            double sum_materiali = sum_nailon + sum_boq + sum_razreditel + 0.40;
+            double sum_maistori = (sum_materiali * 0.3) * hours;
+
+            Console.WriteLine(sum_materiali+sum_maistori);
+        }
+    }
+}
