e6b4c198e66b3c85fe74d863f03508a6ecae13bd
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-08-17 22:24:43 +0300

    Add files via upload

---
+++ striung_numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace striung_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.WriteLine(n[i]);
+            }
+        }
+    }
+}
