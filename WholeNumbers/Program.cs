3c4be6119b4672b3e63ce52584aa2bcaf09fa0a6
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-08-20 11:37:35 +0300

    Add files via upload

---
+++ WholeNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WholeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                numbers.Add(a);
+            }
+
+            int min = numbers[0];
+            int max = numbers[0];
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] > max) max = numbers[i];
+                if (numbers[i] < min) min = numbers[i];
+            }
+            Console.WriteLine($"Max number: {max}");
+            Console.WriteLine($"Min number: {min}");
+        }
+    }
+}
