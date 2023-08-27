14399888753daec4fed806f0ad30c86dbda3486d
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-08-27 14:07:41 +0300

    Add files via upload

---
+++ Ex5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goal = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int total = 0;
+            while (input != "closed" && total<goal)
+            {
+                if (total >= goal) break;
+                string job = input;
+                string specifics = Console.ReadLine();
+                if (job == "haircut")
+                {
+                    if (specifics == "mens") total += 15;
+                    else if (specifics == "ladies") total += 20;
+                    else total += 10;
+                }
+                else
+                {
+                    if (specifics == "touch up") total += 20;
+                    else total += 30;
+                }
+                if (total >= goal) break;
+                input = Console.ReadLine();
+            }
+            if (total >= goal)
+            {
+                Console.WriteLine("You have reached your target for the day!");
+                Console.WriteLine($"Earned money: {total}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Target not reached! You need {(goal-total)}lv. more.");
+                Console.WriteLine($"Earned money: {total}lv.");
+            }
+            
+        }
+    }
+}
---
+++ Ex6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int K = int.Parse(Console.ReadLine());
+            int L = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+            int countCombinations = 0;
+            for (int k = K; k <= 8; k++)
+            {
+                for (int l = 9; l>=L; l--)
+                {
+                    for (int m = M; m <= 8; m++)
+                    {
+                        for (int n = 9; n >= N; n--)
+                        {
+                            if (k % 2 == 0 && l % 2 != 0 && m % 2 == 0 && n % 2 != 0)
+                            {
+                                if (countCombinations < 6)
+                                {
+                                    if (k == m && l == n) Console.WriteLine("Cannot change the same player.");
+                                    else
+                                    {
+                                        Console.WriteLine($"{k}{l} - {m}{n}");
+                                        countCombinations++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
