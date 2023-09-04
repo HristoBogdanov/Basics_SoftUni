84b408bd366a88ea4cbe3ef229d1ab4d44f28f49
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-09-04 16:49:50 +0300

    Add files via upload

---
+++ Combinations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int q = 0; q <= n; q++)
+                    {
+                        if (i + j + q == n) count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
