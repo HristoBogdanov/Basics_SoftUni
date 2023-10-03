baa42bea21ee239aee627e132545a3d5c33c8a83
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-10-03 19:19:51 +0300

    Add files via upload

---
+++ Sum Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = int.Parse(Console.ReadLine());
+
+            while (number>sum)
+            {
+                int input = int.Parse(Console.ReadLine());
+                sum += input;
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
