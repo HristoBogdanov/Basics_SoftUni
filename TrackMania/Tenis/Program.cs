ff1b442c000fdbea9f5380def007fd721099bebc
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-10-03 19:08:32 +0300

    Add files via upload

---
+++ TrackMania/Tenis/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Tenis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int start = int.Parse(Console.ReadLine());
+            double points_generated=0;
+            double won_count = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                string result = Console.ReadLine();
+                if (result == "W")
+                {
+                    start += 2000;
+                    points_generated += 2000;
+                    won_count++;
+                }
+                else if(result=="F")
+                {
+                    start += 1200;
+                    points_generated += 1200;
+                }
+                else if (result == "SF")
+                {
+                    start += 720;
+                    points_generated += 720;
+                }
+            }
+            Console.WriteLine($"Final points: {start}");
+            Console.WriteLine($"Average points: {Math.Floor(points_generated/count)}");
+            Console.WriteLine($"{((won_count/count)*100):f2}%");
+        }
+    }
+}
---
+++ TrackMania/TrackMania/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrackMania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> groups = new List<int>();
+            int count = int.Parse(Console.ReadLine());
+            double Musala = 0;
+            double Monblon = 0;
+            double Kalimandjaro = 0;
+            double K2 = 0;
+            double Everest = 0;
+            double total =0;
+            for (int i = 0; i < count; i++)
+            {
+                int group = int.Parse(Console.ReadLine());
+                groups.Add(group);
+            }
+            foreach (int x in groups)
+            {
+                if (x <= 5) Musala += x;
+                else if (x > 5 && x <= 12) Monblon += x;
+                else if (x > 12 && x <= 25) Kalimandjaro += x;
+                else if (x > 25 && x <= 40) K2 += x;
+                else Everest += x;
+
+                total += x;
+            }
+
+            Console.WriteLine($"{((Musala/total)*100):f2}%");
+            Console.WriteLine($"{((Monblon / total) * 100):f2}%");
+            Console.WriteLine($"{((Kalimandjaro / total) * 100):f2}%");
+            Console.WriteLine($"{((K2 / total) * 100):f2}%");
+            Console.WriteLine($"{((Everest / total) * 100):f2}%");
+        }
+    }
+}
