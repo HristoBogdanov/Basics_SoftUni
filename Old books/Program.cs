bab514bfe538a95c7824f361f8d3072fbcb7efa9
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-10-03 19:22:34 +0300

    Add files via upload

---
+++ Old books/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Old_books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wanted_book = Console.ReadLine();
+            int checked_books = 0;
+            string book_searched = Console.ReadLine();
+            while ( book_searched != "No More Books")
+            {
+                if (wanted_book == book_searched)
+                {
+                    Console.WriteLine($"You checked {checked_books} books and found it.");
+                    break;
+                }
+                else
+                {
+                    checked_books++;
+                    book_searched = Console.ReadLine();
+                }
+            }
+            if (book_searched == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {checked_books} books.");
+            }
+            
+        }
+    }
+}
---
+++ Vacation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = double.Parse(Console.ReadLine());
+            double money = double.Parse(Console.ReadLine());
+            int count = 0;
+            int spend_count = 0;
+            string action;
+            double sum;
+            while (spend_count < 5 && money<price)
+            {
+                action = Console.ReadLine();
+                sum = double.Parse(Console.ReadLine());
+                count++;
+
+                if (action == "spend")
+                {
+                    if (sum > money)money= 0;
+                    else money -= sum;
+
+                    spend_count++;
+                   
+                }
+                else
+                {
+                    money += sum;
+                    spend_count = 0;
+                }
+            }
+            if (spend_count == 5)
+            {
+                Console.WriteLine("You can't save the money.");
+                Console.WriteLine($"{count}");
+            }
+            else
+            {
+                Console.WriteLine($"You saved the money for {count} days.");
+            }
+        }
+    }
+}
